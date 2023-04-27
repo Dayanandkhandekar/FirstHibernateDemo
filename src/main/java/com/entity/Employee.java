b0d054088dbc714ce5aaeac560df2f8a8ca5de1a
Dayanand khandekar <Dayanandkhandekar123@gmail.com>
2023-04-27 18:57:34 +0530

    dd

---
+++ src/main/java/com/entity/Employee.java
@@ -0,0 +1,56 @@
+package com.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "EMP_INFO")
+public class Employee {
+
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "emp_id")
+	int empId;
+	
+	@Column(name = "emp_name")
+	String empName;
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + "]";
+	}
+
+	public Employee(int empId, String empName) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+	}
+	
+	public Employee () {
+		
+	}
+	 
+}
